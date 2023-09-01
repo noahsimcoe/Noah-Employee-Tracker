c0af6ff78a463afe180042d0cea9534a150aa665
noahsimcoe <109931528+noahsimcoe@users.noreply.github.com>
2023-09-01 10:33:17 -0400

    added more file setup and started created inquirer.prompt piece of code

---
+++ server.js
@@ -0,0 +1,9 @@
+// things I need to do:
+
+// import inquirer and fs
+// set up prompt questions and answers from answer bank
+    // see if you can dynamically change question choices in inquirer (based on database)
+// write the different queries to call
+// create schema.sql and seeds.sql to set up the database/tables/values
+// create initial application setup on server.js file
+// download mysql2
