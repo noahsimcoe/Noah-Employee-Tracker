09c2c74b30f8ef8a9bfb12b6c8c934f4420310bf
noahsimcoe <109931528+noahsimcoe@users.noreply.github.com>
2023-09-19 16:07:38 -0500

    fake data from seeds.sql file is not populating correctly

--- schema.sql
+++ schema.sql
@@ -12,22 +12,19 @@ CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     department_id INT,
-    salary DECIMAL(5,2),
+    salary DECIMAL(8,2),
     FOREIGN KEY (department_id)
         REFERENCES department(id)
         ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
     FOREIGN KEY (role_id)
         REFERENCES role(id)
-        ON DELETE SET NULL
-    FOREIGN KEY (manager_id)
-        REFERENCES emploter(id)
         ON DELETE SET NULL
 );
--- seeds.sql
+++ seeds.sql
@@ -1,28 +1,28 @@
-INSERT INTO department (department_name)
+INSERT INTO department(department_name)
 VALUES ("Engineering"),
        ("Marketing"),
        ("Finance"),
        ("Economics");
 
-INSERT INTO role (title, department_id, salary)
-VALUES ("Software Engineer Asc", 1, 75000.00),
-       ("Software Engineer Adv", 1, 93000.00),
+INSERT INTO role(title, department_id, salary)
+VALUES ("Software Engineer Asc", 1, 75000),
+       ("Software Engineer Adv", 1, 93000),
        ("Tech Manager", 1, 105000.00),
        ("Advertising Agent I", 2, 55000.00),
        ("Advertising Agent II", 2, 65000.00),
        ("Analyst Asc", 3, 60000.00),
        ("Analyst Adv", 3, 85000.00),
-       ("Economist I", , 100000.00),
-       ("Economist II", 1, 110000.00),
-       ("Economist Lead", 1, 125000.00);
+       ("Economist I", 4, 100000.00),
+       ("Economist II", 4, 110000.00),
+       ("Economist Lead", 4, 125000.00);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Eli", "Stenstrom", 9),
-VALUES ("Chase", "Hickman", 2),
-VALUES ("Zackum", "Hohenstein", 3, 1),
-VALUES ("Toby", "Young", 5),
-VALUES ("Joel", "Brutlag", 1),
-VALUES ("Cody", "White", 4, 1),
-VALUES ("Adam", "Hollmann", 7),
-VALUES ("Adam", "Mount", 8),
-VALUES ("Jakeum", "Thompson", 10);
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES ("Eli", "Stenstrom", 9, null),
+       ("Chase", "Hickman", 2, 1),
+       ("Zackum", "Hohenstein", 3, 1),
+       ("Toby", "Young", 5, 1),
+       ("Joel", "Brutlag", 1, 1),
+       ("Cody", "White", 4, 1),
+       ("Adam", "Hollmann", 7, 1),
+       ("Adam", "Mount", 8, 1),
+       ("Jakeum", "Thompson", 10, 1);
