f87c134a3fefc961fa99b43bcffd95607148a7ee
noahsimcoe <109931528+noahsimcoe@users.noreply.github.com>
2023-09-20 17:47:35 -0500

    added a few more queries to server.js file, including one for sum of department

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const questions = () => { inquirer.prompt([
             "View All Departments",
             "Add Department",
             "Remove Department",
-            "View Total Utilized Budget By Department",
+            "View Total Budget By Department",
         ],
     },
 ]).then((answers) => {
@@ -39,9 +39,9 @@ const questions = () => { inquirer.prompt([
         case "Add Employee":
             addEmployee();
             break;
-        // case "Remove Employee":
-        //     removeEmployee();
-        //     break;
+        case "Remove Employee":
+            removeEmployee();
+            break;
         // case "Update Employee Role":
         //     updateEmployeeRole();
         //     break;
@@ -51,40 +51,50 @@ const questions = () => { inquirer.prompt([
         case "View All Roles":
             viewAllRoles();
             break;
-        // case "Add Role":
-        //     addRole();
-        //     break;
-        // case "Remove Role":
-        //     removeRole();
-        //     break;
+        case "Add Role":
+            addRole();
+            break;
+        case "Remove Role":
+            removeRole();
+            break;
         case "View All Departments":
             viewAllDepartments();
             break;
-        // case "Add Department":
-        //     addDepartment();
-        //     break;
-        // case "Remove Department":
-        //     removeDepartment();
-        //     break;
-        // case "View Total Utilized Budget By Department":
-        //     viewTotlaUtilizedBudgetByDepartment();
-        //     break;
+        case "Add Department":
+            addDepartment();
+            break;
+        case "Remove Department":
+            removeDepartment();
+            break;
+        case "View Total Budget By Department":
+            viewTotalBudgetByDepartment();
+            break;
         default:
             console.log("Please choose a correct action.");
-            questions();
         }
     });
 };
 
 const viewAllEmployees = async () => {
-        db.query('SELECT CONCAT (first_name, " ", last_name) AS employee FROM employee ORDER BY first_name ASC', function (err, results) {
+        db.query('SELECT id, CONCAT (first_name, " ", last_name) AS employee FROM employee ORDER BY id ASC', function (err, results) {
             console.table(results);
+            questions();
           });
 };
 
-// viewAllEmployeesByDepartment()
-// viewAllEmployeesByManager()
-
+const removeEmployee = () => {
+    inquirer.prompt([
+        {
+            name: "employee_id",
+            type: "input",
+            message: "Enter the employee ID of the employee you would like to remove:",
+        },
+]).then((answer) => {
+    let newData = [answer.employee_id]
+    db.query('DELETE FROM employee WHERE id = ?', newData, (err, results) => {
+        viewAllEmployees();
+    });
+})};
 
 const addEmployee = () => {
     inquirer.prompt([
@@ -115,29 +125,106 @@ const addEmployee = () => {
     });
 })};
 
-// removeEmployee()
+const removeRole = () => {
+    inquirer.prompt([
+        {
+            name: "role_id",
+            type: "input",
+            message: "Enter the role ID of the role you would like to remove:",
+        },
+]).then((answer) => {
+    let newData = [answer.role_id]
+    db.query('DELETE FROM role WHERE id = ?', newData, (err, results) => {
+        viewAllRoles();
+    });
+})};
+
 // updateEmployeeRole()
 // updateEmployeeManager()
+// viewAllEmployeesByDepartment()
+// viewAllEmployeesByManager()
+
+
+const viewTotalBudgetByDepartment = () => {
+    inquirer.prompt([
+        {
+            name: "department_id",
+            type: "input",
+            message: "What is the ID of the department you are looking for?",
+        },
+]).then((answer) => {
+    let newData = [answer.department_id]
+    db.query('SELECT SUM(salary) AS total_annual_salaries FROM role r INNER JOIN employee e ON r.id = e.role_id WHERE department_id = ?', newData, (err, results) => {
+        console.table(results);
+        questions();
+    });
+})};
+
 
 const viewAllRoles = async () => {
-    db.query('SELECT title, salary FROM role ORDER BY salary DESC', function (err, results) {
+    db.query('SELECT id, title, salary FROM role ORDER BY id DESC', function (err, results) {
         console.table(results);
+        questions();
     });
 }
 
-// addRole()
-// removeRole()
-
+const addRole = () => {
+    inquirer.prompt([
+        {
+            name: "title",
+            type: "input",
+            message: "What is the title of the new role?",
+        },
+        {
+            name: "department_id",
+            type: "input",
+            message: "What is the department ID of the new role?",
+        },
+        {
+            name: "salary",
+            type: "input",
+            message: "What is the salary of the new role?"
+        },
+]).then((answer) => {
+    let newData = [answer.title, answer.department_id, answer.salary]
+    db.query('INSERT INTO role (title, department_id, salary) VALUES (?, ?, ?)', newData, (err, results) => {
+        viewAllRoles();
+    });
+})};
 
 const viewAllDepartments = async () => {
-    db.query('SELECT department_name FROM department ORDER BY department_name ASC', function (err, results) {
+    db.query('SELECT id, department_name FROM department ORDER BY id ASC', function (err, results) {
         console.table(results);
+        questions();
     });
 }
 
+const addDepartment = () => {
+    inquirer.prompt([
+        {
+            name: "department_name",
+            type: "input",
+            message: "What is the name of the new department?",
+        },
+]).then((answer) => {
+    let newData = [answer.department_name]
+    db.query('INSERT INTO department (department_name) VALUES (?)', newData, (err, results) => {
+        viewAllDepartments();
+    });
+})};
 
-// addDepartment()
-// removeDepartment()
-// viewTotlaUtilizedBudgetByDepartment()
+const removeDepartment = () => {
+    inquirer.prompt([
+        {
+            name: "department_id",
+            type: "input",
+            message: "Enter the department ID of the department you would like to remove:",
+        },
+]).then((answer) => {
+    let newData = [answer.department_id]
+    db.query('DELETE FROM department WHERE id = ?', newData, (err, results) => {
+        viewAllDepartments();
+    });
+})};
 
 questions();
