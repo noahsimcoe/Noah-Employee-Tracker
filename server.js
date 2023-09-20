8a901810342820e6936d963720ddb541055a41b5
noahsimcoe <109931528+noahsimcoe@users.noreply.github.com>
2023-09-20 16:50:11 -0500

    added three more queries for the CLI

--- server.js
+++ server.js
@@ -29,16 +29,16 @@ const questions = () => { inquirer.prompt([
     switch (answers.start) {
         case "View All Employees":
             viewAllEmployees();
-        //     break;
+            break;
         // case "View All Employees by Department":
         //     viewAllEmployeesByDepartment();
         //     break;
         // case "View All Employees By Manager":
         //     viewAllEmployeesByManager();
         //     break;
-        // case "Add Employee":
-        //     addEmployee();
-        //     break;
+        case "Add Employee":
+            addEmployee();
+            break;
         // case "Remove Employee":
         //     removeEmployee();
         //     break;
@@ -48,18 +48,18 @@ const questions = () => { inquirer.prompt([
         // case "Update Employee Manager":
         //     updateEmployeeManager();
         //     break;
-        // case "View All Roles":
-        //     viewAllRoles();
-        //     break;
+        case "View All Roles":
+            viewAllRoles();
+            break;
         // case "Add Role":
         //     addRole();
         //     break;
         // case "Remove Role":
         //     removeRole();
         //     break;
-        // case "View All Departments":
-        //     viewAllDepartments();
-        //     break;
+        case "View All Departments":
+            viewAllDepartments();
+            break;
         // case "Add Department":
         //     addDepartment();
         //     break;
@@ -71,13 +71,12 @@ const questions = () => { inquirer.prompt([
         //     break;
         default:
             console.log("Please choose a correct action.");
+            questions();
         }
     });
 };
 
 const viewAllEmployees = async () => {
-        // const sql = `SELECT first_name, last_name FROM employee ORDER BY first_name ASC`;
-        // console.log(db.query(sql));
         db.query('SELECT CONCAT (first_name, " ", last_name) AS employee FROM employee ORDER BY first_name ASC', function (err, results) {
             console.table(results);
           });
@@ -85,14 +84,58 @@ const viewAllEmployees = async () => {
 
 // viewAllEmployeesByDepartment()
 // viewAllEmployeesByManager()
-// addEmployee()
+
+
+const addEmployee = () => {
+    inquirer.prompt([
+        {
+            name: "first_name",
+            type: "input",
+            message: "What is the employee's first name?",
+        },
+        {
+            name: "last_name",
+            type: "input",
+            message: "What is the employee's last name?",
+        },
+        {
+            name: "role",
+            type: "input",
+            message: "What is the Role ID of the employees's role?"
+        },
+        {
+            name: "manager",
+            type: "input",
+            message: "What is the Manager ID of the employees's manager?"
+        },
+]).then((answer) => {
+    let newData = [answer.first_name, answer.last_name, answer.role, answer.manager]
+    db.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)', newData, (err, results) => {
+        viewAllEmployees();
+    });
+})};
+
 // removeEmployee()
 // updateEmployeeRole()
 // updateEmployeeManager()
-// viewAllRoles()
+
+const viewAllRoles = async () => {
+    db.query('SELECT title, salary FROM role ORDER BY salary DESC', function (err, results) {
+        console.table(results);
+    });
+}
+
 // addRole()
 // removeRole()
-// viewAllDepartments()
+
+
+const viewAllDepartments = async () => {
+    db.query('SELECT department_name FROM department ORDER BY department_name ASC', function (err, results) {
+        console.table(results);
+    });
+}
+
+
 // addDepartment()
 // removeDepartment()
 // viewTotlaUtilizedBudgetByDepartment()
